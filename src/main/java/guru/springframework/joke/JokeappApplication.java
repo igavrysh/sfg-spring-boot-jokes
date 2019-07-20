fbb636a2e7295a24622f6aff50d7621fedffc945
Ievgen Gavrysh <ievgen_gavrysh@epam.com>
2019-07-20 07:22:21 -0700

    Add xml-based spring configuration, chuckNorrisQuotes bean, wire new bean in JokeserviceImpl

--- src/main/java/guru/springframework/joke/JokeappApplication.java
+++ src/main/java/guru/springframework/joke/JokeappApplication.java
@@ -2,12 +2,14 @@ package guru.springframework.joke;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-public class SfgSpringBootJokesApplication {
+@ImportResource("classpath:chuck-config.xml")
+public class JokeappApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SfgSpringBootJokesApplication.class, args);
+        SpringApplication.run(JokeappApplication.class, args);
     }
 
 }
